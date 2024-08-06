4be647b16120576779856bddf4b1b094ca932b91
Iram Luis Morales Santos <iramluism@gmail.com>
2024-08-06 05:45:37 +0200

    feat: abort on container exit

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 docker compose -f ./deployments/docker-compose.tests.yml build
-docker compose -f ./deployments/docker-compose.tests.yml up -d
+docker compose -f ./deployments/docker-compose.tests.yml up -d --abort-on-container-exit
 docker compose -f ./deployments/docker-compose.tests.yml logs run_tests
-docker compose -f ./deployments/docker-compose.tests.yml down 
